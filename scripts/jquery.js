19aa31fc80573236387ea14a64004d5080c7d0d6
Rodolphe Geant <rodolphe@rgeant.name>
2016-06-22 17:40:09 +0400

    EY FO 777
    added EY F/O experience

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
   $(".ey").hide();
+
   $("#eyCaptainTitle").click(function(){
     $("#eyCaptain").toggle();
     $("#ey_777").toggle();
+  })
+
+   $("#eyFoTitle").click(function(){
+    $("#eyFo").toggle();
+    $("#ey_777_2").toggle();
   })
 });
